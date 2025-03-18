d356f3c788f018d3a403967dd403b3af91225e03
rac@racc.com <rac@racc.com>
2025-03-18 13:18:06 -0700

    Revert "Minor updates."
    This reverts commit f4b7a35bb931af5f5c94f92f7fda8116ce2942a2.

--- src/main/java/Team4450/Robot25/RobotContainer.java
+++ src/main/java/Team4450/Robot25/RobotContainer.java
@@ -268,7 +268,6 @@ public class RobotContainer
 		elevator.setDefaultCommand(new RunCommand(
 		 	()->{elevator.move(-MathUtil.applyDeadband(utilityController.getLeftY() * 0.5, DRIVE_DEADBAND));
 		 	}, elevator));
-			
 		//Start the compressor, PDP and camera feed monitoring Tasks.
 
    		monitorCompressorThread = MonitorCompressorPH.getInstance(pcm);
@@ -440,6 +439,7 @@ public class RobotContainer
 
 		new Trigger(() -> driverController.getAButton())
     		.toggleOnTrue(new RetractClimber(climber));
+		
 			
 		// -------- Utility pad buttons ----------
 		
